c3c394fb67321a914982114ac7f7c09de98222a2
tujiaw@163.com <tujiaw@163.com>
2024-12-21 17:50:07 +0800

    feat: 更新博客内容和标签数据
    - 移除了 Main 组件中的 Latest 标题
    - 更新了 tag-data.json 中的标签数据
    - 修正了多篇博客文章中的代码格式问题
    - 更新了 siteMetadata 中的博客标题和描述

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,8 +2,8 @@
 const siteMetadata = {
   title: 'Next.js Starter Blog',
   author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  headerTitle: "Ningto's Blog",
+  description: 'Welcome to my blog! Stay Hungry, Stay Foolish.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
